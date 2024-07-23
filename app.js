4c86b586aec4adacb93b8c0d7181e6bb818db645
Sayed <sayed.sais@gmail.com>
2024-07-23 14:18:20 +0100

    completed user story 1 epic 2

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.use(helmet());
 app.get("/", (req, res) => {
   res.status(200).send("Hello world!");
 });
+let data = [{
+	"id": "54321234", // UUID
+	"activity_submitted": "1719486190058", // simple Epoc timestamp (Date.now() in JS)
+	"activity_type": "run", // choose some standard types
+	"activity_duration": "30", // choose standard unit type (minutes probably)
+}]
+
+app.get("/activities", (req, res) => {
+  res.status(200).send(data)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
